1c9457baebf72460812ef63c21eca9e06d24961c
Guilherme Silva Andre <guilhermeaandre2018@gmail.com>
2025-04-04 12:32:23 -0300

    Começando a fazer o ex7

---
+++ Exercicios/Ex7/calcular.php
@@ -0,0 +1,14 @@
+<?php
+$palavra = $_POST['palavra'] ?? '';
+$quantidade = $_POST['quantidade'] ?? 0;
+
+echo "<h2>Resultado:</h2>";
+
+if (is_numeric($quantidade) && $quantidade > 0) {
+    for ($i = 1; $i <= $quantidade; $i++) {
+        echo "$i - $palavra <br>";
+    }
+} else {
+    echo "Quantidade inválida.";
+}
+?>
