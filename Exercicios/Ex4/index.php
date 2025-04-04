3065c6173a818cedf10a39e0764ca2d01763ac9c
Samuel Santana Torriani <samuel.st.2204@gmail.com>
2025-04-04 12:23:04 -0300

    Ex4 Feito

---
+++ Exercicios/Ex4/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercicio 04</title>
+</head>
+<body>
+    <h2>Exercicio 4:</h2>
+    <form action="calcular.php" method="post">
+        <label for="frase">Digite a frase:</label><br>
+        <input type="text" id="frase" name="frase" required><br><br>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
