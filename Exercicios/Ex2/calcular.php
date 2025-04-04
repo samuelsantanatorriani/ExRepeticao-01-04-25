4e8f06cfbebd54b6954cb3b9c985cdba79419548
Samuel Santana Torriani <samuel.st.2204@gmail.com>
2025-04-04 12:04:46 -0300

    Ex2 feito

---
+++ Exercicios/Ex2/calcular.php
@@ -0,0 +1,11 @@
+<?php
+$i = 0;
+
+ echo "<h2>Resultado:</h2>";
+
+ for ($i = 0; $i < 11; $i++)
+ {
+    echo "$i<br>";
+ }
+  
+?>
---
+++ Exercicios/Ex2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercício 02</title>
+</head>
+<body>
+    <h2>Exercicio 02</h2>
+    <form action="calcular.php" method="post">
+        <input type="submit" value="Mostrar numeros">
+    </form>
+</body>
+</html>
