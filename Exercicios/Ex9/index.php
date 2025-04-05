e775f14740fd2258e30ee64edd34250d910772b7
Guilhermesilvaandre <guilhermeaandre2018@gmail.com>
2025-04-04 22:18:24 -0300

    ex 9 feito

---
+++ Exercicios/Ex9/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercicio 09</title>
+</head>
+<body>
+    <h2>Exercicio 9:</h2>
+    <form action="calcular.php" method="post">
+        <label for="n1">Digite o número:</label><br>
+        <input type="text" id="n1" name="n1" required><br><br>
+
+
+        <label for="n2">Digite o intervalo dos números:</label><br>
+        <input type="text" id="n2" name="n2" required><br><br>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
